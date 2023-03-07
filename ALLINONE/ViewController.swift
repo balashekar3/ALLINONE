9d7a7fb926f36027eb1112a74a7a11f6560e3b01
balashekar3 <balashekar3@gmail.com>
2023-03-07 19:56:24 +0530

    initial commit

---
+++ ALLINONE/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ALLINONE
+//
+//  Created by Balashekar Vemula on 07/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
